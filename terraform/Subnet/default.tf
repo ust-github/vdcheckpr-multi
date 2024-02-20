699cb31cbf20c54db87a62be77da68ebf70e5d4e
Edge Opsx <edgeops@ust-global.com>
2024-02-20 13:56:48 +0400

    VDCHECKPR-00: Delete resource demo

--- terraform/Subnet/default.tf
+++
@@ -1,9 +0,0 @@
-module "subnet" {
-  source = "terraform-aws-modules/subnet/aws"
-  name = "my-subnet"
-  cidr = "10.0.0.0/16"
-  tags = {
-    Terraform = "true"
-    Environment = "qa"
-  }
-}
--- terraform/VPC/default.tf
+++
@@ -1,9 +0,0 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
